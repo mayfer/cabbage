a9aa2a95a2323e661544e39837ed29f464183b4f
Theo Stavrides <theostavrides@gmail.com>
2020-04-12 00:47:13 -0700

    add prompt component

---
+++ client/components/prompt.js
@@ -0,0 +1,57 @@
+define(function(require, exports) {
+
+    const css = require('core/css')
+    const { Component, render, html, useRef, h } = require('preact');
+    const DrawingCanvas = require("components/canvas/draw");
+    const TextInput = require("components/textInput");
+
+    class Prompt extends Component {
+        constructor(props) {
+            super();
+            let channel = props.channel;
+            this.state = {
+            };
+        }
+
+        componentDidMount() {
+        }
+
+        componentWillUnmount() {
+        }
+
+        componentShouldUpdate(){
+        }
+
+        render(props, s) {
+            const { promptMode } = props;
+            return html`
+            	<div id="prompt-container">
+					${promptMode === 'draw' ? html`
+					<${DrawingCanvas} />
+					` : ''}
+					${promptMode === 'text' ? html`
+					<${TextInput} />
+					` : ''}
+					${promptMode === 'textResponse' ? html`
+					<img src=''/>
+					<${TextInput} />
+					` : ''}
+				</div>
+            `
+        }
+
+        static css() {
+            return `
+            	#prompt-container {
+            		padding-top: 20px;
+            	}
+            ` + DrawingCanvas.css() + TextInput.css();
+        }
+
+    }
+
+    return Prompt;
+})
+
+
+
