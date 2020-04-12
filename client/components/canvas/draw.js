7c9229f4c50bae852174afbef72140dfbed6b24a
Theo Stavrides <theostavrides@gmail.com>
2020-04-11 23:20:43 -0700

    remove margin auto from draw component

--- client/components/canvas/draw.js
+++ client/components/canvas/draw.js
@@ -58,7 +58,7 @@ define(function(require, exports) {
 
         static css() {
             return `
-                .drawing-container { width: 600px; margin: 0 auto; padding: 50px; text-align: center; }
+                .drawing-container { width: 600px; padding: 50px 0; text-align: center; }
                 .draw-area { width: 600px; height: 400px; border: 3px solid #000; background: #fff; }
                 .draw-area:hover { cursor: url("/client/assets/pencil.svg") 0 30, progress; }
                 .control-button { margin: 10px; font-size: 15px; line-height: 20px; min-width: 55px; }
