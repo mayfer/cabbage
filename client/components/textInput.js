bd4f6be5adfc25ec3a81213b10b569c64362199f
Theo Stavrides <theostavrides@gmail.com>
2020-04-12 00:47:25 -0700

    add textInput component

--- client/components/textInput.js
+++ client/components/textInput.js
@@ -41,7 +41,7 @@ define(function(require, exports) {
 
         static css() {
             return `
-            	.main-textarea { width: 100%; padding-top: 50px; display: flex; align-items: center; flex-flow: column;}
+            	.main-textarea { width: 100%; display: flex; align-items: center; flex-flow: column;}
             	.main-textarea textarea { width: 100%; height: 300px; resize: none; border: 2px solid black; font-size: 30px; padding: 20px; box-sizing: border-box;}
             	.main-textarea textarea:focus { outline: none; }
             `;
