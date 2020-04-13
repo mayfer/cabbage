62971d9dfedd83c20f47704140da47c21e881307
Madeleine Francis <madeleinefrncs@gmail.com>
2020-04-12 21:39:20 -0700

    added timeSince and createStack functions to lobby - still a general dumpster fire though

--- client/components/lobby.js
+++ client/components/lobby.js
@@ -18,7 +18,7 @@ define(function(require, exports) {
 
         render(props, s) {
             const { channel } = props;
-            var dummyData = [{name: "murat", count: 3}, {name: "adele", count: 2}, {name: "theo", count: 7 }];
+            var dummyData = [{name: "murat", count: 3, lastTime: Date.now()}, {name: "adele", count: 2, lastTime: Date.now()}, {name: "theo", count: 7, lastTime: Date.now() }];
             return html`
             <div id="lobby">
                 <div class="rounds-content">
@@ -28,18 +28,6 @@ define(function(require, exports) {
                         </div>
                         <div class="rounds">
                             
-                            ${dummyData.map(d => html`
-                                <a href='/'>
-                                    <div class="stack" >
-                                        ${d.name}
-                                        <div class="paper-sheet"> </div>
-                                        <div class="stack-count" > 
-                                            ${d.count}
-                                        </div>
-                                    </div>
-                                </a>
-                            `)}
-                            
                             <div class="stack" >
                                 <a 
                                     href='/lobby/${channel}/round/new/'
@@ -48,8 +36,6 @@ define(function(require, exports) {
                                     <button>Add Round +</button>
                                 </a>
                             </div>
-                            
-                            
                         </div>
                     </div>
                     <div class="complete-rounds-wrapper">
@@ -59,9 +45,9 @@ define(function(require, exports) {
                         <div class="rounds">
                             ${dummyData.map(d => html`
                                 <a href='/'>
-                                    <div class="stack" >
-                                        ${d.name}
-                                        <img class="stack-image" src="/client/assets/stack1.svg" />
+                                    <div class="stack paper-stack-wrapper" >
+                                        ${this.createPaperStack(d.count)}
+                                        ${this.timeSince(d.lastTime)}
                                     </div>
                                 </a>
                             `)}
@@ -81,14 +67,49 @@ define(function(require, exports) {
                 .stack-image { width: 100px}
                 .round-section-title { margin-top: 50px; margin-bottom: 15px;}
                 .footer-image { width: 100%; }
-                .stack { position: relative; text-align:center; height: 67px; width: 100px;}
+                .stack { height: 100px; width: 100px;}
                 .stack-image { width: 100%; position: absolute; }
                 .stack-count {postion: absolute}
-                .paper-sheet {background-color: grey; width: 100%}
+                .paper-sheet {position: absolute; box-shadow: inset 0 0 10px #000; background-color: #f5f3f3; width: 50px; height: 50px;}
+                .paper-stack-wrapper { position: relative; width: 100px; height: 100px;}
 
             `
         }
 
+        timeSince(date) {
+            var seconds = Math.floor((new Date() - date) / 1000);
+            var interval = Math.floor(seconds / 31536000);
+
+            if (interval > 1) {
+            return interval + " years";
+            }
+            interval = Math.floor(seconds / 2592000);
+            if (interval > 1) {
+            return interval + " months";
+            }
+            interval = Math.floor(seconds / 86400);
+            if (interval > 1) {
+            return interval + " days";
+            }
+            interval = Math.floor(seconds / 3600);
+            if (interval > 1) {
+            return interval + " hours";
+            }
+            interval = Math.floor(seconds / 60);
+            if (interval > 1) {
+            return interval + " minutes";
+            }
+            return Math.floor(seconds) + " seconds";
+        }
+
+        createPaperStack(count) {
+            var countArray = Array(count).fill(null)
+            return countArray.map((d, i) => {
+                return html `
+                <div class="paper-sheet" style="left:${i * 2}px; bottom:${i * 3}px">
+                </div>`
+            })
+        }
     }
 });
 
